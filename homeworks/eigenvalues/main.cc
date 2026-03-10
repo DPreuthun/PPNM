a435917a54da6db5967ebf5f1d5d0c2cb0089210
DPreuthun <au751556@uni.au.dk>
2026-03-10 12:56:22 +0100

    update

--- homeworks/eigenvalues/main.cc
+++ homeworks/eigenvalues/main.cc
@@ -1,11 +1,12 @@
 #include "vecmatrix.h"
 #include "jacobi.h"
 #include <iostream>
+#include <fstream>
 #include <iomanip>
-#include <random>
 #include <cmath>
+#include <string>
 
-int main() {
+int main(int argc,char** argv) {
     std::cout << std::fixed << std::setprecision(6);
 
     // A1
@@ -63,7 +64,94 @@ int main() {
 
     std::cout << "V * V^T = " << "\n" << V * V.transpose() << "\n";
 
-    
+    // B
+    std::cout << "----------B----------" << "\n";
 
+    double rmax = 10;
+    double dr = 0.3;
+
+    for(int i=1;i<argc;i++){
+        std::string arg = argv[i];
+
+        if(arg=="-rmax") rmax = atof(argv[++i]);
+        if(arg=="-dr")   dr   = atof(argv[++i]);
+    }
+
+    int npoints = (int)(rmax/dr) - 1;
+
+    pp::vector r(npoints);
+
+    for(int i=0;i<npoints;i++)
+        r[i] = dr*(i+1);
+    
+    pp::matrix H(npoints,npoints);
+
+    for(int i=0;i<npoints-1;i++){
+
+        H(i,i)   = -2*(-0.5/dr/dr);
+
+        H(i,i+1) =  1*(-0.5/dr/dr);
+
+        H(i+1,i) =  1*(-0.5/dr/dr);
+    }
+
+    H(npoints-1,npoints-1) = -2*(-0.5/dr/dr);
+
+    for(int i=0;i<npoints;i++)
+    H(i,i) += -1.0/r[i];
+
+    pp::matrix Vh(npoints,npoints);
+
+    pp::jacobi(H,Vh);
+
+    double Emin = H(0,0);
+    int index = 0;
+
+    for(int i=1;i<npoints;i++){
+        if(H(i,i) < Emin){
+            Emin = H(i,i);
+            index = i;
+        }
+    }
+
+    pp::vector psi(npoints);
+
+    int i1=0, i2=0, i3=0;
+    double e1=1e100, e2=1e100, e3=1e100;
+
+    for(int i=0;i<npoints;i++){
+        double e = H(i,i);
+
+        if(e < e1){
+            e3=e2; i3=i2;
+            e2=e1; i2=i1;
+            e1=e;  i1=i;
+        }
+        else if(e < e2){
+            e3=e2; i3=i2;
+            e2=e;  i2=i;
+        }
+        else if(e < e3){
+            e3=e;  i3=i;
+        }
+    }
+
+    std::cout << "\nThree lowest eigenvalues:\n";
+    std::cout << "E1 = " << e1 << "\n";
+    std::cout << "E2 = " << e2 << "\n";
+    std::cout << "E3 = " << e3 << "\n";
+
+    std::cout << "\nFirst 10 components of the corresponding eigenvectors:\n";
+
+    for(int i=0;i<10 && i<npoints;i++){
+        std::cout << "i=" << i
+                << "  psi1=" << Vh(i,i1)
+                << "  psi2=" << Vh(i,i2)
+                << "  psi3=" << Vh(i,i3)
+                << "\n";
+    }
+
+    std::cout << "\nExact ground state energy = -0.5\n";
+    
     return 0;
 }
